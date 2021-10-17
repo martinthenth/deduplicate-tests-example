6ccbef06ca79c6ab139d70b0a57777c316fc33c7
martinthenth <m.r.nijboer@thables.com>
2021-10-17 23:56:16 +0200

    feat: update `Users.User` schema

--- lib/deduplicate/users/user.ex
+++ lib/deduplicate/users/user.ex
@@ -1,20 +1,37 @@
 defmodule Deduplicate.Users.User do
   use Ecto.Schema
+
   import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
+  alias __MODULE__
+
+  @primary_key {:user_id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
-    field :age, :integer
     field :name, :string
+    field :password, :string, virtual: true, redact: true
+    field :password_hash, :string
+    field :is_banned, :boolean
 
     timestamps()
   end
 
   @doc false
+  @spec changeset(%User{}, map) :: Ecto.Changeset.t()
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :age])
-    |> validate_required([:name, :age])
+    |> cast(attrs, [:name, :password, :is_banned])
+    |> validate_required([:name])
+    |> validate_length(:password, min: 8, max: 100)
+    |> hash_password()
   end
+
+  @doc false
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    changeset
+    |> change(Argon2.add_hash(password))
+    |> delete_change(:password)
+  end
+
+  defp hash_password(changeset), do: changeset
 end
