8bff1c2fb82146395de8079051377cf56c390e27
martinthenth <m.r.nijboer@thables.com>
2021-10-18 20:58:38 +0200

    feat: implement `UserController.update/2` and `~/delete/2` in router.ex

--- lib/deduplicate_web/router.ex
+++ lib/deduplicate_web/router.ex
@@ -1,8 +1,15 @@
 defmodule DeduplicateWeb.Router do
   use DeduplicateWeb, :router
 
+  alias DeduplicateWeb.Plugs
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :authenticated do
+    plug Plugs.GuardianPipeline
+    plug Plugs.CurrentUser
   end
 
   # Public routes.
@@ -10,6 +17,13 @@ defmodule DeduplicateWeb.Router do
     pipe_through :api
 
     post "/users/create", UserController, :create
+  end
 
+  # Authenticated routes.
+  scope "/api/users/:user_id", DeduplicateWeb do
+    pipe_through [:api, :authenticated]
+
+    post "/update", UserController, :update
+    post "/delete", UserController, :delete
   end
 end
