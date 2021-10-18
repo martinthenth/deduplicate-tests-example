a29110ff5e25bb4b57ad1c9a90b1e244103ba38e
martinthenth <m.r.nijboer@thables.com>
2021-10-18 21:44:10 +0200

    test: add comments to `AuthenticationTestsMacro` and `UserControllerTest`

--- test/support/macros/authentication_tests_macro.ex
+++ test/support/macros/authentication_tests_macro.ex
@@ -1,7 +1,7 @@
 defmodule DeduplicateWeb.AuthenticationTestsMacro do
   @moduledoc """
-  This module defines helper functions for testing authentication
-  and authorization requirements for controller actions.
+  This module defines macros for generating authentication
+  tests for controller functions.
   """
 
   import ExUnit.Assertions
@@ -11,22 +11,27 @@ defmodule DeduplicateWeb.AuthenticationTestsMacro do
   to unauthenticated and unauthorized users.
   """
   @spec test_user_authentication(atom, binary) :: any
-  defmacro test_user_authentication(action, path) do
+  # Define macro.
+  defmacro test_user_authentication(http_method, route_path) do
+    # Mark the code as `generated`.
     quote generated: true do
       import Deduplicate.UsersFixtures
       import Deduplicate.Users.SessionsFixtures
 
       alias Deduplicate.Users
 
-      @action unquote(action)
-      @path unquote(path)
+      # Make `http_method` and `route_path` available in the test blocks.
+      @http_method unquote(http_method)
+      @route_path unquote(route_path)
 
+      # Implement the first test.
       test "user is not authenticated, renders error", %{conn: conn} do
         assert conn
-               |> fetch_response(@action, @path)
+               |> fetch_response(@http_method, @route_path)
                |> json_response(401)
       end
 
+      # Implement the second test.
       test "user is banned, renders error", %{conn: conn} do
         user = user_fixture()
         token = session_fixture(user)
@@ -35,10 +40,11 @@ defmodule DeduplicateWeb.AuthenticationTestsMacro do
 
         assert conn
                |> put_req_header("authorization", token)
-               |> fetch_response(@action, @path)
+               |> fetch_response(@http_method, @route_path)
                |> json_response(401)
       end
 
+      # Implement the third test.
       test "user is deleted, renders error", %{conn: conn} do
         user = user_fixture()
         token = session_fixture(user)
@@ -47,11 +53,13 @@ defmodule DeduplicateWeb.AuthenticationTestsMacro do
 
         assert conn
                |> put_req_header("authorization", token)
-               |> fetch_response(@action, @path)
+               |> fetch_response(@http_method, @route_path)
                |> json_response(401)
       end
 
       @doc false
+      # Make an HTTP-request to the route `path`,
+      # by pattern matching on the `http_method` variable.
       defp fetch_response(conn, :get, path), do: get(conn, path)
       defp fetch_response(conn, :post, path), do: post(conn, path)
     end
