223412d464158c685d8a41c77986c98fee03f059
martinthenth <m.r.nijboer@thables.com>
2021-10-18 20:58:14 +0200

    feat: update `UserController` and add `delete/2` action

--- lib/deduplicate_web/controllers/user_controller.ex
+++ lib/deduplicate_web/controllers/user_controller.ex
@@ -9,7 +9,7 @@ defmodule DeduplicateWeb.UserController do
   @doc """
   Creates a user with the given params.
   """
-  @spec create(Plug.Conn.t(), map) :: Plug.Conn.t() | {:error, Ecto.Changeset.t()}
+  @spec create(Plug.Conn.t(), map) :: Plug.Conn.t() | {:error, Ecto.Changeset.t() | atom}
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = created_user} <- Users.create_user(user_params) do
       conn
@@ -23,7 +23,7 @@ defmodule DeduplicateWeb.UserController do
   @doc """
   Updates a user with the given params.
   """
-  @spec update(Plug.Conn.t(), map) :: Plug.Conn.t() | {:error, Ecto.Changeset.t()}
+  @spec update(Plug.Conn.t(), map) :: Plug.Conn.t() | {:error, Ecto.Changeset.t() | atom}
   def update(conn, %{"user" => user_params}) do
     user = conn.assigns.current_user
 
@@ -35,4 +35,20 @@ defmodule DeduplicateWeb.UserController do
   end
 
   def update(_conn, _params), do: {:error, :bad_request}
+
+  @doc """
+  Deletes a user with the given params.
+  """
+  @spec delete(Plug.Conn.t(), map) :: Plug.Conn.t() | {:error, Ecto.Changeset.t() | atom}
+  def delete(conn, %{"user_id" => _}) do
+    user = conn.assigns.current_user
+
+    with {:ok, %User{} = deleted_user} <- Users.delete_user(user) do
+      conn
+      |> put_status(:accepted)
+      |> render("show.json", user: deleted_user)
+    end
+  end
+
+  def delete(_conn, _params), do: {:error, :bad_request}
 end
--- test/deduplicate_web/controllers/user_controller_test.exs
+++ test/deduplicate_web/controllers/user_controller_test.exs
@@ -3,19 +3,14 @@ defmodule DeduplicateWeb.UserControllerTest do
 
   import Deduplicate.UsersFixtures
 
-  alias Deduplicate.Users.User
+  alias Deduplicate.Users
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "create/2" do
-    @valid_params %{
-      user: %{
-        name: "some name",
-        password: "some password"
-      }
-    }
+    @valid_params %{user: %{name: "some name", password: "some password"}}
     @missing_params %{}
 
     test "with valid params, creates a user, renders the user", %{conn: conn} do
@@ -30,6 +25,89 @@ defmodule DeduplicateWeb.UserControllerTest do
       conn = post(conn, path, @missing_params)
 
       assert json_response(conn, 400)
+    end
+  end
+
+  describe "update/2" do
+    import Deduplicate.UsersFixtures
+    import Deduplicate.Users.SessionsFixtures
+
+    setup do
+      user = user_fixture()
+      token = session_fixture(user)
+
+      %{user: user, token: token}
+    end
+
+    @valid_params %{user: %{name: "updated name", password: "updated password", is_banned: true}}
+    @missing_params %{}
+
+    test "with valid params, updates the user, renders the user", %{
+      conn: conn,
+      user: user,
+      token: token
+    } do
+      path = Routes.user_path(conn, :update, user.user_id)
+
+      conn =
+        conn
+        |> put_req_header("authorization", token)
+        |> post(path, @valid_params)
+
+      assert %{"user" => _} = json_response(conn, 202)["data"]
+
+      # Verify user is updated with given data.
+      updated_user = Users.get_user!(user.user_id)
+
+      assert updated_user.password_hash != user.password_hash
+      assert updated_user.name == @valid_params.user.name
+      assert updated_user.is_banned == @valid_params.user.is_banned
+    end
+
+    test "with missing params, renders error", %{conn: conn, user: user, token: token} do
+      path = Routes.user_path(conn, :update, user.user_id)
+
+      conn =
+        conn
+        |> put_req_header("authorization", token)
+        |> post(path, @missing_params)
+
+      assert json_response(conn, 400)
+
+      # Verify user was not updated.
+      assert Users.get_user!(user.user_id) == user
+    end
+  end
+
+  describe "delete/2" do
+    import Deduplicate.UsersFixtures
+    import Deduplicate.Users.SessionsFixtures
+
+    setup do
+      user = user_fixture()
+      token = session_fixture(user)
+
+      %{user: user, token: token}
+    end
+
+    test "with valid params, deletes the user, renders the user", %{
+      conn: conn,
+      user: user,
+      token: token
+    } do
+      path = Routes.user_path(conn, :delete, user.user_id)
+
+      conn =
+        conn
+        |> put_req_header("authorization", token)
+        |> post(path)
+
+      assert %{"user" => _} = json_response(conn, 202)["data"]
+
+      # Verify user is deleted.
+      assert_raise Ecto.NoResultsError, fn ->
+        Users.get_user!(user.user_id)
+      end
     end
   end
 end
